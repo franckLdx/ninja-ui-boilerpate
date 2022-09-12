e56e000d19eb6e8c50df8f45b1dc243f6da47a02
Utilisateur <franck.ledoux.pro@gmail.com>
2022-09-12 19:55:56 +0200

    update

--- src/components/button/Button.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import styles from './button.module.scss';
-export const Button = () => (React.createElement("button", { className: styles.button }, "HELLO"));
--- src/components/input/Input.js
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export const Input = () => React.createElement("input", null);
